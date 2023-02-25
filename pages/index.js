6d5fafbaf40a93ce7eab44ea256e154d14d46851
Michal Varys <Sufursky@gmail.com>
2023-02-25 10:40:53 +0100

    Initial commit

---
+++ pages/index.js
@@ -0,0 +1,5 @@
+import { HomeScreen } from "../src/sections/homepage/HomeScreen";
+
+export default function Page() {
+  return <HomeScreen />;
+}
---
+++ src/sections/homepage/HomeScreen.jsx
@@ -0,0 +1,13 @@
+import { Center, Heading } from "@chakra-ui/layout";
+
+export function HomeScreen() {
+  return (
+    <Center w="100vw" h="100vh">
+      <Heading fontSize="5xl" fontWeight="extrabold">
+        Hello World
+      </Heading>
+
+      <div>home</div>
+    </Center>
+  );
+}
